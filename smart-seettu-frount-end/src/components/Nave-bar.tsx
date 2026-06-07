1c09a7ac893307e867651ea92347fc8789ce70fc
savindunawanjana <savindunawanjana08@gmail.com>
2026-06-08 00:50:29 +0530

    update some

--- smart-seettu-frount-end/src/components/Nave-bar.tsx
+++ smart-seettu-frount-end/src/components/Nave-bar.tsx
@@ -118,14 +118,14 @@ const Header = () => {
               onClick={closeLoginFromBackground}
             >
               <div
-                className="w-full max-w-md md:max-w-xl bg-cyan-950 flex flex-col gap-1 p-6 sm:p-10 md:pl-11 border rounded-3xl mx-auto w-25px h-[50vh] mt-10 mb-10 ml-5 mr-5"
+                className="w-full lg:h-[60vh] sm:w-[80vw] sm:h-[70vh] md:h-[70vh] max-w-md md:max-w-xl bg-cyan-950 flex flex-col gap-1 p-6 sm:p-10 md:pl-11 border rounded-3xl mx-auto w-25px h-[50vh] mt-10 mb-10 ml-5 mr-5"
                 onClick={(e) => e.stopPropagation()}
               >
                 <label
                   htmlFor=""
                   className=" sm:text-4xl md:text-4xl pt-1 sm:p:7"
                 >
-                  Please Log in
+                  Please Login
                 </label>
                 <div className="flex flex-col gap-2">
                   <label
@@ -168,8 +168,10 @@ const Header = () => {
                   className="btn btn-outline-primary bg-transparent border-amber-400 text-amber-400 font-bold rounded border-1  h-[6vh] w-[90%]"
                 >
                   Register for free
-                </button>
-                <span className="text-xl ">OR</span>
+                </button>{" "}
+                <span className="text-xl  h-[6vh] w-[90%] flex items-center justify-center ">
+                  <h1>OR</h1>
+                </span>
                 <button
                   type="button"
                   className="btn btn-outline-primary bg-amber-300  h-[6vh] border-b-blue-950 border-1 rounded hover:bg-amber-400 text-black font-bold w-[90%] mb-5"
